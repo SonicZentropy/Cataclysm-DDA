7f7345b53406ee7ff2cad892e30df722bb5e0274
Casey Bailey <caseybailey@gmail.com>
2026-05-06 04:11:21 -0500

    Labs Return

--- src/omdata.h
+++ src/omdata.h
@@ -478,6 +478,8 @@ struct oter_type_t {
         void check() const;
         void finalize();
 
+
+
         bool is_rotatable() const {
             return !has_flag( oter_flags::no_rotate ) && !has_flag( oter_flags::line_drawing );
         }
@@ -601,6 +603,8 @@ struct oter_t {
         bool has_flag( oter_flags flag ) const {
             return type->has_flag( flag );
         }
+
+    bool is_hardcoded() const;
 
         bool is_rotatable() const {
             return type->is_rotatable();
