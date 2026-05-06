ad6f31c1ae8c6b9ef74527332018d30eb4a93f88
Casey Bailey <caseybailey@gmail.com>
2026-05-06 04:12:15 -0500

    No zombie revives

--- src/item.cpp
+++ src/item.cpp
@@ -2643,7 +2643,8 @@ bool item::ready_to_revive( map &here, const tripoint_bub_ms &pos ) const
             }
         }
 
-        return true;
+        //return true;
+        return false;
     }
     return false;
 }
