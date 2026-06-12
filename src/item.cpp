84983fe8e802d07659e5d3f58105381b3414e229
Zentropy Software <zentropysoft@proton.me>
2026-06-11 19:20:51 -0500

    Moved Zombie Revival option to a mod, so you can choose

--- src/item.cpp
+++ src/item.cpp
@@ -2645,8 +2645,7 @@ bool item::ready_to_revive( map &here, const tripoint_bub_ms &pos ) const
             }
         }
 
-        //return true;
-        return false;
+        return true;
     }
     return false;
 }
