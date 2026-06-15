b0b9f0a373901c563c7a7e82406a1b017de7b09d
Zentropy Software <zentropysoft@proton.me>
2026-06-14 19:53:19 -0500

    Remove achievements disabling by debug mode

--- src/debug_menu.cpp
+++ src/debug_menu.cpp
@@ -4745,11 +4745,11 @@ namespace debug_menu {
         static const std::unordered_set<debug_menu_index> non_cheaty_options = {
             debug_menu_index::ENABLE_ACHIEVEMENTS
         };
-        if (!is_debug_character() && !non_cheaty_options.count(action) &&
-            achievements.is_enabled()) {
-            add_msg(m_mixed, _("Achievements have been disabled."));
-            achievements.set_enabled(false);
-        }
+        // if (!is_debug_character() && !non_cheaty_options.count(action) &&
+        //     achievements.is_enabled()) {
+        //     add_msg(m_mixed, _("Achievements have been disabled."));
+        //     achievements.set_enabled(false);
+        // }
 
         get_event_bus().send<event_type::uses_debug_menu>(action);
 
